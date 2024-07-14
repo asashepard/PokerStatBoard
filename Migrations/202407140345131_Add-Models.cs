ff0f2f969742a63f31f4705ed51909a1f913b5ea
Asa Shepard <asa.shepard@gmail.com>
2024-07-13 22:46:22 -0500

    Add models for game, player, buy-in, cash-out

---
+++ Migrations/202407140345131_Add-Models.cs
@@ -0,0 +1,61 @@
+﻿namespace PokerStatBoard.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddModels : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BuyInModels",
+                c => new
+                    {
+                        BuyInID = c.Guid(nullable: false),
+                        PokerGameID = c.Guid(nullable: false),
+                        Amount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        DateTime = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.BuyInID);
+            
+            CreateTable(
+                "dbo.CashOutModels",
+                c => new
+                    {
+                        CashOutID = c.Guid(nullable: false),
+                        PokerGameID = c.Guid(nullable: false),
+                        Amount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        DateTime = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.CashOutID);
+            
+            CreateTable(
+                "dbo.PlayerModels",
+                c => new
+                    {
+                        PlayerID = c.Guid(nullable: false),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.PlayerID);
+            
+            CreateTable(
+                "dbo.PokerGameModels",
+                c => new
+                    {
+                        PokerGameID = c.Guid(nullable: false),
+                        StartDateTime = c.DateTime(nullable: false),
+                        EndDateTime = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.PokerGameID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.PokerGameModels");
+            DropTable("dbo.PlayerModels");
+            DropTable("dbo.CashOutModels");
+            DropTable("dbo.BuyInModels");
+        }
+    }
+}
---
+++ Models/CashOutModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PokerStatBoard.Models
+{
+    public class CashOutModel
+    {
+        [Key]
+        public Guid CashOutID { get; set; }
+
+        [Required]
+        public Guid PokerGameID { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        public CashOutModel(Guid PokerGameID, decimal Amount)
+        {
+            CashOutID = Guid.NewGuid();
+            this.PokerGameID = PokerGameID;
+            this.Amount = Amount;
+            DateTime = DateTime.Now;
+        }
+    }
+}
