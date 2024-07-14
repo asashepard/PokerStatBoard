596cca3e92e03e4ac148f3210be54cd6c7a3f35c
Asa Shepard <asa.shepard@gmail.com>
2024-07-13 22:26:36 -0500

    Create initial database

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace PokerStatBoard.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PokerStatBoard.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PokerStatBoard.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
