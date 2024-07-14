5a21cd8d0bb6a3b1ca02411e40ab196e1ebac559
Asa Shepard <asa.shepard@gmail.com>
2024-07-14 00:33:58 -0500

    Add CurrentGame to database

---
+++ Models/CurrentGameModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+
+namespace PokerStatBoard.Models
+{
+    public class CurrentGameModel
+    {
+        [Key]
+        public Guid CurrentGameID { get; set; }
+        
+        public Guid PokerGameID { get; set; }
+    }
+}
