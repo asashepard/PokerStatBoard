aaff29e21812895dae4b88c11dfead755e6ad6dd
Asa Shepard <asa.shepard@gmail.com>
2024-07-14 00:25:31 -0500

    Add end game button and skeleton functionality (not working)

---
+++ Controllers/EndGameController.cs
@@ -0,0 +1,31 @@
+﻿using PokerStatBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PokerStatBoard.Controllers
+{
+    public class EndGameController : Controller
+    {
+        public ActionResult Index()
+        {
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+
+            foreach (PokerGameModel model in dbContext.PokerGames)
+            {
+                if (model.EndDateTime > DateTime.Now) // current game
+                {
+                    model.EndDateTime = DateTime.Now;
+
+                    dbContext.SaveChanges();
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
