9d561641dfa06c2c7c4d7d81e1f5f19bdc902f30
Asa Shepard <asa.shepard@gmail.com>
2024-07-15 23:08:56 -0500

    Add email confirmation and password verification

--- Global.asax.cs
+++ Global.asax.cs
@@ -16,6 +16,7 @@ namespace PokerStatBoard
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            EmailServiceCredentials.PopulateEmailCredentialsFromAppConfig();
         }
     }
 }
